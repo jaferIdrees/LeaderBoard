b77516f68ab54a18d2efc4bf1b0180f32d791da6
jaferIdrees <jaferll@gmail.com>
2022-07-21 13:59:42 +0300

    Apply final styling

--- src/DynamicHTML.js
+++ src/DynamicHTML.js
@@ -1,5 +1,6 @@
 export const populateScores = (scoresArray) => {
   const scoresTable = document.getElementById('scores_table');
+  scoresArray.sort((a, b) => (a.score > b.score ? -1 : 1));
   scoresTable.innerHTML = '';
   for (let i = 0; i < scoresArray.length; i += 1) {
     const tr = document.createElement('tr');
